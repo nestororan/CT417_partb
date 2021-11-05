a305658e1765e52421e2514651082ff38090276b
nestororan <nestororan100@gmail.com>
2021-11-05 14:15:59 +0000

    changed driver class

---
+++ src/main/java/com/github/nestororan/gradleproject2/driverClass.java
@@ -0,0 +1,26 @@
+package com.github.nestororan;
+
+import org.joda.time.DateTime;
+
+import java.util.ArrayList;
+
+public class driverClass {
+    Student joseph;
+    Student alex;
+    Student shane;
+
+    Module databaseSys;
+    Module Comp101;
+
+
+    Programme p1 ;
+    Programme CS;
+
+
+
+    public void makeStudents() {
+     
+        joseph=new Student("joseph",20,"5/7/2000",123456,ECE,new List<String>("databaseSys","Comp101"))
+        
+    }
+}
